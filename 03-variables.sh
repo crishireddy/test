ad177835742169581f4da84e3ce8f41311fb415c
crishireddy <chimullarishireddy02@gmail.com>
2022-01-18 14:39:54 -0500

    modified new file  03-variable.sh

--- 03-variables.sh
+++ 03-variables.sh
@@ -21,3 +21,7 @@ echo $NAME
 #FILE2=new.txt
 
 #cp $FILE1 $FILE2
+# if you have space in between commands use " or else it will throw an error when u print
+#NAME=HELLO WORLD (result will say error in line 25 to avoid that use "
+NAME="HELLO WORLD"
+echo NAME
