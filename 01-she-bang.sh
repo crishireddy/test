d2f37736fb630488dd50b9a681787ecde9813153
crishireddy <chimullarishireddy02@gmail.com>
2022-01-17 21:45:27 -0500

    added a file by name she-bang.sh

---
+++ 01-she-bang.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#1 #1.#! is used to denote the path of the interpretor
+   #meaning when you give she-bang the shell scripting creats a path for a series of commands to excute one after another
+   #in a given line.
+
+#2."commands will be excuted using she-bang" in simple terms.
+
+#3. all the above line starting with # are comments which are description for commands we use we can write why we used
+    #such command for which work for later understand
+
+ls
+find /etc -name passwd
