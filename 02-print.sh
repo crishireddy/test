62676511854152f29141b35c017328683c4f5cd2
crishireddy <chimullarishireddy02@gmail.com>
2022-01-18 12:42:17 -0500

    modified a file in 02-print.sh

--- 02-print.sh
+++ 02-print.sh
@@ -4,3 +4,15 @@
 #echo is widly used and easy to use.
 echo Hello world
 echo welcome to Devops
+# we can print colour in server by using esc sequence in echo here
+#esc sequence are /n for new line
+#                 /e for colours
+# Note when ever we use esc seq the echo codes should be in double colons
+
+
+echo Welcome to devops training
+#above will print in one single line
+echo "Hello, /nWelcome to devops training "
+#will print in 2 seperate line like
+#hello
+#welcome to devops training when we pull in server
