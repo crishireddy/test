dc6b686173860d58e7709d9722d1c635941c9ab1
crishireddy <chimullarishireddy02@gmail.com>
2022-01-18 13:36:26 -0500

    modified in 02-print.sh

--- 02-print.sh
+++ 02-print.sh
@@ -6,8 +6,10 @@ echo Hello world
 echo welcome to Devops
 # we can print colour in server by using esc sequence in echo here
 #esc sequence are \n for new line
-#                 \e for colours \e[col-codemmessages
+#                 "\e for colours \e[COL-codemmessages"
 #                 \t for tab space
+#                 "\e[PROP-COL-CODE;COL-CODEmMESSAGES" (properties meaning to bold or underline or etc a text
+#                 "\e[0m" (to disable the colour to default.
 # Note when ever we use esc seq the echo codes should be in double " and have to enable -e
 
 
@@ -18,5 +20,22 @@ echo -e "Hello,\nWelcome to devops training"
 #hello
 #welcome to devops training when we pull in server.
 echo -e "\e[33mHello \nHow are you"
-echo -e "\e[32mI am Rishi \n\e[33mlearning Devops
-"
+echo -e "\e[32mI am Rishi \n\e[33mlearning Devops"
+
+#   color           code
+#   Red             31
+#   green           32
+#   yellow          33
+#   blue            34
+#   magenta         35
+#   cyan            36
+echo bye
+#the above bye will print in yellow as colour code doesnt disabble automatically
+# we have to give a code to disable the colour and more code to
+#display text in bold underline etc..
+
+#   code        purpose
+#   0           reset the colour to default
+#   1           bold the text
+#   4           underline the text
+
