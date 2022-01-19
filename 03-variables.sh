845bd608240fdf4224d4a088a1b5b65046e1aedc
crishireddy <chimullarishireddy02@gmail.com>
2022-01-18 22:21:31 -0500

    modified new file  03-variables.sh

--- 03-variables.sh
+++ 03-variables.sh
@@ -31,7 +31,7 @@ echo "count of Apples =${count}no"
 # why we used { was to get count of apples = 2no as result if without {} then it would print count of apples =     ,
 #as it would take countno as one thing but we gave count=2 and we anted to print 2no as result so adding {} would brake
 #count and no without space.
-FULL NAME=RISHI
+FULLNAME=RISHI
 count=3
-echo "$FULL NAME"
+echo "$FULLNAME"
 echo "count of cherry =${count}no"
