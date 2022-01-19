b31068f696bb7c794c0cd84ecf5341923da8dd82
crishireddy <chimullarishireddy02@gmail.com>
2022-01-18 21:56:01 -0500

    modified new file  03-variables.sh

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,7 +4,7 @@
 #declare a variable in bash shell
 NAME=DEVOPS
 #in above variable is name and value is devops
-#variable name = data devops
+#variable is name = data is devops
 
 
 #
@@ -12,7 +12,7 @@ NAME=DEVOPS
 
 #How to acess the variable we use ${var_name} symbol if we dont use $ then it will print just NAME insted of DEVOPS
 
-echo ${NAME}
+echo "${NAME}"
 #here when we do sh 03-variable then insted of name it will print DEVOPS
 
 #another example
@@ -24,7 +24,7 @@ echo ${NAME}
 # if you have space in between commands use " or else it will throw an error when u print
 #NAME=HELLO WORLD (result will say error in line 25 to avoid that use "
 NAME="HELLO WORLD"
-echo ${NAME}
+echo "${NAME}"
 
 count=2
 echo "count of Apples =${count}no"
