a8209b025b7bd2ccdc10b68a81ecf3fec1afc249
crishireddy <chimullarishireddy02@gmail.com>
2022-01-19 22:30:57 -0500

    modified new file  03-variables.sh

--- 04-read.sh
+++ 04-read.sh
@@ -2,8 +2,8 @@
 
 #read command prompts some message as question to which viewer answers
 
-read-p 'ENTER YOUR NAME: ' name
-read-p 'ENTER YOUR AGE: ' age
+read -p 'ENTER YOUR NAME: ' name
+read -p 'ENTER YOUR AGE: ' age
 
 #echo -e \n\e[31myour name ="${name}" \e[0m \nyour age ="${age}"
 
