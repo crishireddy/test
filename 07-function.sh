44c7ef70ac7de482bb8e4bcfa81288c2fd0d6a95
crishireddy <chimullarishireddy02@gmail.com>
2022-01-20 21:47:37 -0500

    edited new file  07-function.sh

--- 07-function.sh
+++ 07-function.sh
@@ -6,8 +6,11 @@ apple () {
   echo hello from function
 echo hello world
 echo FIRST ARGUMENT = $1
-echo SECOND ARGUMENT = "$2"
-}
 
+}
+sample () {
+  echo SECOND ARGUMENT = "$2"
+}
 #sample xyz
 apple abc
+sample xyz
