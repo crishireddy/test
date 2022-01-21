60ee537341e495fbefacf86ec1a1523b54aaba31
crishireddy <chimullarishireddy02@gmail.com>
2022-01-20 22:02:49 -0500

    edited new file  07-function.sh

--- 07-function.sh
+++ 07-function.sh
@@ -6,13 +6,18 @@ apple () {
   echo hello from function
 echo hello world
 echo FIRST ARGUMENT = $1
-
 }
 sample () {
  echo hello
 
 echo SECOND ARGUMENT = $1
 }
-#sample xyz
+#in functions if you dont give arguments below it wont excute, as first all the function pacakage goes into memory
+#and then the cpu process the first excution would be apple and second will be sample, once it excutes apple
+#it brings the apple() function into work and prints all the info from it, after function excution it reads the argument
+#and prints the argument accordingly.
 apple abc
 sample xyz
+# note if we generate functiona and if we dont give the main excution below like apple(){ and then we give
+#apple if we miss this the it wont excute it will just store the function pacakage into cpu and search for excution
+#command.
