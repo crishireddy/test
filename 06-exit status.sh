9416ed682dfdab78ddf911089847acaf646380d7
crishireddy <chimullarishireddy02@gmail.com>
2022-01-20 21:23:49 -0500

    created new file  06-exit status.sh

--- 06-exit status.sh
+++ 06-exit status.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#echo Hello # It will excute hello
-#ext #exit commands exits the terminal it will not print the next command bye in Environment.
-#echo bye # it never prints.
+echo Hello # It will excute hello
+exit #exit commands exits the terminal it will not print the next command bye in Environment.
+echo bye # it never prints.
 
-echo hello
-exit
-echo bye
+#echo hello
+#exit
+#echo bye
