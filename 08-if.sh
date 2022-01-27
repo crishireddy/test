afdf98bb6337624cdfcad1175e769baee100ffbf
crishireddy <chimullarishireddy02@gmail.com>
2022-01-26 23:35:30 -0500

    modified a new file 08.if.sh

--- 08-if.sh
+++ 08-if.sh
@@ -3,7 +3,7 @@
 # now we are starting with if statement.
 #in if statements space are important to follow [ abc ]
 
-if [ "$USER" != "root" ] then
+if [ "$USER" != "root" ]; then
   echo Hey your not a root user
 fi
 
