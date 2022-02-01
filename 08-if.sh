ecc451feac47f5f4364a6825936c652ea8267fb5
crishireddy <chimullarishireddy02@gmail.com>
2022-01-31 23:44:24 -0500

    updated  a new file 08-if.sh

--- 08-if.sh
+++ 08-if.sh
@@ -9,6 +9,12 @@ fi
 
 #if is closed by fi
 
+#in the below statement am trying to tell the difference between read.sh and if statement
+#in read we used this string of read -p 'enter name .... but in execution if we leave the name
+#blank it wont shoot any thing where as in if statement we can give the output we want in prior
+#so if name given show one thing else show another thing (name not given)
+
+#what is a -z
 
 read -p 'Enter your name: ' name
 if [ -z "$name" ]; then
@@ -18,3 +24,7 @@ else
 fi
 
 # when we use = have to know
+
+if [ -z "$a" ]; then
+  echo a is empty
+fi
